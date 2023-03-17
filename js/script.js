e80652b034569f7bd306f6e33d5e84c01e577a3e
“jdelavega94” <jcdelavega94@gmail.com>
2023-03-17 19:13:58 +0100

    add responsive columns

--- js/script.js
+++ js/script.js
@@ -6,19 +6,22 @@ play.addEventListener("click", function(){
     const mode = difficulty.value;
     const numberOfSquares = squareRegulator(mode);
     const numbers = myArrayGenerator(numberOfSquares);
+    grid.innerHTML= '';
     for (let i = 0; i < numbers.length; i++){
         const currentNumber = numbers[i];
-        const newBox = boxGenerator(currentNumber);
-        newBox.addEventListener("click", handleDivClick);
-        grid.append(newBox);
+        const newGrid = boxGenerator(currentNumber);
+        newGrid.addEventListener("click", handleDivClick);
+        grid.append(newGrid);
+        console.log(newGrid);
     }
+    // grillRegulator();
 })
 
 // !FUNCTIONS
 // #@general
 function myArrayGenerator(totalSquare){
     const myArray = [];
-    for(let i = 0; myArray.length < totalSquare; i++) {
+    for(let i = 1; myArray.length < totalSquare; i++) {
         const myNumber = i;
         myArray.push(myNumber);
     }
@@ -39,12 +42,28 @@ function squareRegulator(anyMode){
 // @dom related function
 
 function boxGenerator(content){
+    const mode = difficulty.value;
+    const dif = grillRegulator(mode)
     const newDiv = document.createElement("div");
-    newDiv.classList.add("box");
+    newDiv.classList.add("box", dif);
     newDiv.innerHTML = `<span>${content}</span>`;
     return newDiv;
 }
 
 function handleDivClick(){
+    console.log(this.textContent);
     this.classList.add("azure");
+}
+
+function grillRegulator(anyItem){
+    let columns = '';
+    if(anyItem === "easy") {
+        columns = "column10"
+    } else if (anyItem === "medium"){
+        columns = "column9"
+    } else if (anyItem === "hard"){
+        columns = "column7"
+    }
+    return columns;
+
 }
